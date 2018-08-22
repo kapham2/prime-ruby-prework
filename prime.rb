ed048990e4c3453f5e2ec96bf51630cd38b6af1d
Kim Pham <pham.kim.a@gmail.com>
2018-08-22 14:48:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,17 +7,15 @@ def prime?(num)
     return true
   else
     number_array = (2..(num**0.5).to_int).to_a
-    prime_number = []
-    
+
     number_array.each do |number|
       
       if num % number == 0
-        puts num
-        puts number
-        puts num % number
         return false
       end
+      
     end
+    
     return true
   end
 end
